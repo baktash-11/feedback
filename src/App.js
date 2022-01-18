4d850f2a05ef77e15d994c8d52b6eb26da87863f
Baktash Sana <baktash11@gmail.com>
2022-01-17 21:54:36 -0500

    comments list added dynamically to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 function App(){
+    // using variables dynamically 
+    let title = "My App";
+    let body ="This is my blog Post!"
+
+    // creating a list dynamically
+    let comments = [
+        {id: 1, text: "Comment one"},
+        {id: 2, text: "Comment two"},
+        {id: 3, text: "Comment three"},
+        {id: 4, text: "Comment four"}
+    ]
     return(
         <div className="container">
-            <h1>Welcome to my Feed back app!</h1>
-            <p>this is the nwe app for feed back in react</p>
-
+            <h1>{title}!</h1>
+            <p>{body}</p>
+            <h3>Number of Comments: {comments.length}</h3>
+            <div className="comment">
+                <ul>
+                    {comments.map((comment, index)=>(
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
