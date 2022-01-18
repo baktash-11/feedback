1a92b1604bfa958bcd877380e2f6dfea60e00b18
Baktash Sana <baktash11@gmail.com>
2022-01-18 12:22:30 -0500

    state has been created and added as rating and comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
 import PropTypes from 'prop-types'
 
-function Header(props) {
+function Header({text, bgColor, textColor}) {
+    const headerStyle = {
+        backgroundColor: bgColor, 
+        color: textColor,
+    }
     return (
-        <header>
+        <header style={headerStyle}>
             <div className="container">
-                <h2>{props.text}</h2>
+                <h2>{text}</h2>
             </div>
         </header>
         
     )
 }
 Header.defaultProps ={
-    text: 'Feedback UI'
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.5)',
+    textColor:"#ff6a95",
 }
+
 Header.propTypes={
-    text: PropTypes.string
+    text: PropTypes.string,
+    bgColor:PropTypes.string, 
+    textColor: PropTypes.string,
+
 }
 
 export default Header
