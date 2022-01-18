1b69f4450d1c07b342af28da7e932eb10239ec5a
Baktash Sana <baktash11@gmail.com>
2022-01-17 21:43:19 -0500

    App file component has been created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App(){
+    return(
+        <div className="container">
+            <h1>Welcome to my Feed back app!</h1>
+            <p>this is the nwe app for feed back in react</p>
+
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import App from "./App"
 
 
-reactDom.render(<h1>My App</h1>, document.getElementById('root'));
+// reactDom.render(<h1>My App</h1>, document.getElementById('root'));
+reactDom.render(<App/>, document.getElementById('root'));
