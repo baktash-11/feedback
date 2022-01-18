caf9b93ec21ab8f66ee411775f91c6a37f69e3b1
Baktash Sana <baktash11@gmail.com>
2022-01-18 00:33:43 -0500

    index.css has been updated and components folder has been created. a short intro to props

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-
+import Header  from "./components/Header";
 function App(){
-    // using variables dynamically 
-    let title = "My App";
-    let body ="This is my blog Post!"
-
-    // creating a list dynamically
-    let comments = [
-        {id: 1, text: "Comment one"},
-        {id: 2, text: "Comment two"},
-        {id: 3, text: "Comment three"},
-        {id: 4, text: "Comment four"}
-    ]
-
-    // using condition 
-    let loading= false;
-    if(loading)return <h1>Loading...</h1>
-
-    let commentBlock = (
-        <div className="comment">
-        <h3>Number of Comments: {comments.length}</h3>
-        <ul>
-            {comments.map((comment, index)=>(
-                <li key={index}>{comment.text}</li>
-            ))}
-        </ul>
-    </div>
-    )
-    let showComments = true;
-    let commentMessage = "You have four Comments: "
+    
     return(
-        <div className="container">
-            <h1>{title.toUpperCase()}!</h1>
-            <p>{body}</p>
-            
-            {showComments ? commentBlock : commentMessage+ comments.length}
-
-           
-        </div>
+        <>
+            <Header/>
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+function Header(props) {
+    return (
+        <header>
+            <div className="container">
+                <h2>{props.text}</h2>
+            </div>
+        </header>
+        
+    )
+}
+Header.defaultProps ={
+    text: 'Feedback UI'
+}
+Header.propTypes={
+    text: PropTypes.string
+}
+
+export default Header
+
+// _rfce creating component
+// impt import proptypes 
